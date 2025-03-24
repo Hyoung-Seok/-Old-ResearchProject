ab387e2036d255e2869e9cff9e0e5c554fb1d902
Hyoung-Seok <dkavmdk98@gmail.com>
2025-03-25 01:07:13 +0900

    [Path] 미로 생성 방식 Recursive Backtracking으로 변경

--- Assets/2.Scripts/Algorithm/PathFinding/TileGenerator.cs
+++ Assets/2.Scripts/Algorithm/PathFinding/TileGenerator.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
+
 
 public class TileGenerator : MonoBehaviour
 {
@@ -12,11 +15,16 @@ public class TileGenerator : MonoBehaviour
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private float offset = 0.5f;
-
+    
+    private readonly int[] _dx = new[] { 0, 0, -2, 2 };
+    private readonly int[] _dy = new[] { -2, 2, 0, 0 };
     private int[,] _tile;
-
+    
     private void Start()
     {
+        if (width % 2 == 0) width -= 1;
+        if (height % 2 == 0) height -= 1;
+        
         SetTileArray();
         GenerateTile();
     }
@@ -24,58 +32,56 @@ public class TileGenerator : MonoBehaviour
     private void SetTileArray()
     {
         _tile = new int[height, width];
+        var stack = new Stack<(int, int)>();
         
-        for (var col = 0; col < height; ++col)
+        stack.Push(GetRandomPosition());
+
+        while (stack.Count > 0)
         {
-            for (var row = 0; row < width; ++row)
+            var pos = stack.Pop();
+            _tile[pos.Item1, pos.Item2] = 1;
+
+            var nextPos = GetNextPosition(pos);
+
+            if (nextPos.Item1 == -1)
             {
-                if (col == 0 || col >= height - 1)
-                {
-                    _tile[col, row] = 1;
-                    continue;
-                }
-
-                if (row == 0 || row >= width - 1)
-                {
-                    _tile[col, row] = 1;
-                    continue;
-                }
-
-                if (row % 2 == 0 || col % 2 == 0)
-                {
-                    _tile[col, row] = 1;
-                }
+                continue;
             }
+            
+            stack.Push(pos);
+            stack.Push(nextPos);
         }
+    }
+
+    private (int, int) GetNextPosition((int, int) pos)
+    {
+        var land = new[] { 0, 1, 2, 3 };
+        land = land.OrderBy(_ => Random.value).ToArray();
+
+        foreach (var index in land)
+        {
+            if (pos.Item1 + _dy[index] < 1 || pos.Item1 + _dy[index] >= height - 1 ||
+                pos.Item2 + _dx[index] < 1 || pos.Item2 + _dx[index] >= width - 1 ||
+                _tile[pos.Item1 + _dy[index], pos.Item2 + _dx[index]] == 1)
+            {
+                continue;
+            }
+
+            _tile[pos.Item1 + _dy[index] / 2, pos.Item2 + _dx[index] / 2] = 1;
+            return (pos.Item1 + _dy[index], pos.Item2 + _dx[index]);
+        }
+
+        return (-1, -1);
+    }
+    
+    private (int, int) GetRandomPosition()
+    {
+        var pos = (0, 0);
+
+        pos.Item1 = Random.Range(0, (height - 1) / 2) * 2 + 1;
+        pos.Item2 = Random.Range(0, (width - 1) / 2) * 2 + 1;
         
-        for (var col = 1; col < height; col += 2)
-        {
-            for (var row = 1; row < width; row += 2)
-            {
-                Vector2Int pos;
-                
-                if(col == height - 2 && row == width - 2) continue;
-                
-                if (row == width - 2)
-                {
-                    pos = new Vector2Int(col + 1, row);
-                }
-                else if (col == height - 2)
-                {
-                    pos = new Vector2Int(col, row + 1);
-                }
-                else if (Random.Range(0, 2) == 0)
-                {
-                    pos = new Vector2Int(col, row + 1);
-                }
-                else
-                {
-                    pos = new Vector2Int(col + 1, row);
-                }
-
-                _tile[pos.x, pos.y] = 0;
-            }
-        }
+        return pos;
     }
 
     private void GenerateTile()
@@ -86,7 +92,7 @@ public class TileGenerator : MonoBehaviour
         {
             for (var row = 0; row < width; ++row)
             {
-                var obj = (_tile[col, row] == 1)
+                var obj = (_tile[col, row] == 0)
                     ? Instantiate(wall, transform)
                     : Instantiate(ground, transform);
 
