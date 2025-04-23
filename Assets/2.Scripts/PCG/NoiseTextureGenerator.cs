baeb6f62163668eb0b8d244ae7dc7fc56ffac7d8
Hyoung-Seok <dkavmdk98@gmail.com>
2025-04-23 20:25:02 +0900

    [PCG] FallOffMap 텍스쳐 생성 기능 추가

--- Assets/2.Scripts/PCG/NoiseTextureGenerator.cs
+++ Assets/2.Scripts/PCG/NoiseTextureGenerator.cs
@@ -21,9 +21,31 @@ public static class NoiseTextureGenerator
         tex.filterMode = FilterMode.Point;
         tex.wrapMode = TextureWrapMode.Clamp;
         tex.Apply();
+        
         return tex;
     }
 
+    public static Texture2D GenerateFalloffMapTexture(float[,] falloff)
+    {
+        var tex = new Texture2D(falloff.GetLength(1), falloff.GetLength(0));
+
+        for (var y = 0; y < falloff.GetLength(0); ++y)
+        {
+            for (var x = 0; x < falloff.GetLength(1); ++x)
+            {
+                var val = falloff[y, x];
+                var color = new Color(val, val, val);
+                tex.SetPixel(x, y, color);
+            }
+        }
+        
+        tex.filterMode = FilterMode.Bilinear;
+        tex.wrapMode = TextureWrapMode.Clamp;
+        tex.Apply();
+
+        return tex;
+    }
+    
     private static Color ApplyHeightColor(float height)
     {
         foreach (var colorHeight in _colorHeights)
