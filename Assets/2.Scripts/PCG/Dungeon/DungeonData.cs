c9281ae71fe06b97ddc9979dfdc7fce7057b794b
Hyoung-Seok <dkavmdk98@gmail.com>
2025-05-08 21:53:11 +0900

    [PCG] 방 위치를 저장할 변수 생성

--- Assets/2.Scripts/PCG/Dungeon/DungeonData.cs
+++ Assets/2.Scripts/PCG/Dungeon/DungeonData.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DungeonData", menuName = "PCG/DungeonData")]
 public class DungeonData : ScriptableObject
 {
+    [Header("BSP Setting")]
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private int roomMinWidth;
     [SerializeField] private int roomMinHeight;
     [SerializeField] private int iteration;
 
+    [Header("Room Generate Setting")] 
+    [SerializeField] private Vector2Int offset;
+    [SerializeField, Range(0f, 0.4f)] private float bottomLeftWeight;
+    [SerializeField, Range(0.6f, 0.9f)] private float topRightWeight;
+
     public int Width => width;
     public int Height => height;
     public int RoomMinWidth => roomMinWidth;
     public int RoomMinHeight => roomMinHeight;
     public int Iteration => iteration;
+    public Vector2Int Offset => offset;
+    public float BottomLeftWeight => bottomLeftWeight;
+    public float TopRightWeight => topRightWeight;
 }
