ad9505e0f26120f2e5fe95f0a3241fc8531aca4d
Hyoung-Seok <dkavmdk98@gmail.com>
2025-05-08 21:53:32 +0900

    [PCG] 로직 적용

--- Assets/2.Scripts/PCG/Dungeon/DungeonGenerator.cs
+++ Assets/2.Scripts/PCG/Dungeon/DungeonGenerator.cs
@@ -17,8 +17,38 @@ public class DungeonGenerator : MonoBehaviour
     private void GenerateDungeon()
     {
         var bsp = new BinarySpacePartitioning();
+        var roomGenerator = new RoomGenerator();
+        var lineDisplay = GetComponent<LineDisplay>();
+        
         _roomNodeList = bsp.BSP(dungeonData);
         
-        GetComponent<LineDisplay>().DisplayLine(_roomNodeList[0]);
+        Debug.Log(GetAllLeafNode().Count);
+        roomGenerator.GenerateRoom(GetAllLeafNode(), dungeonData);
+        lineDisplay.DisplayLine(_roomNodeList[0]);
+        lineDisplay.DisplayLine(GetAllLeafNode());
+    }
+
+    private List<RoomNode> GetAllLeafNode()
+    {
+        var leafNodes = new List<RoomNode>();
+        var queue = new Queue<RoomNode>(new[] { _roomNodeList[0] });
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+
+            if (node.ChildNode.Count <= 0)
+            {
+                leafNodes.Add(node);
+                continue;
+            }
+
+            foreach (var chile in node.ChildNode)
+            {
+                queue.Enqueue((RoomNode)chile);
+            }
+        }
+
+        return leafNodes;
     }
 }
