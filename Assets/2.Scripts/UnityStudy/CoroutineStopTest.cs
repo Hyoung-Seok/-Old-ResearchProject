0d126fc31fb366a5ac0b095e1940f2141a02d887
Hyoung-Seok <dkavmdk98@gmail.com>
2025-05-30 21:46:55 +0900

    IEnumerator과 Coroutine 차이 분석

--- Assets/2.Scripts/UnityStudy/CoroutineStopTest.cs
+++ Assets/2.Scripts/UnityStudy/CoroutineStopTest.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class CoroutineStopTest : MonoBehaviour
 {
+    private IEnumerator _testEnumerator;
+    private Coroutine _testCoroutine;
+
+    private WaitForSeconds _waitForSeconds = new WaitForSeconds(1f);
+    
     private void Start()
     {
-        StartCoroutine("TestRoutine");
+        // _testEnumerator = TestRoutine();
+        // StartCoroutine(_testEnumerator);
+        
+        _testCoroutine = StartCoroutine(TestRoutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StopCoroutine(_testCoroutine);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _testCoroutine = StartCoroutine(TestRoutine());
+        }
     }
 
     private IEnumerator TestRoutine()
     {
         var i = 0;
+        Debug.Log("코루틴 시작");
         
-        while (true)
+        while (i < 10)
         {
             Debug.Log(++i);
-
-            if (i > 1000)
-            {
-                Destroy(gameObject);
-            }
-            yield return new WaitForEndOfFrame();
+            yield return _waitForSeconds;
         }
+        
+        Debug.Log("코루틴 중지");
     }
 }
