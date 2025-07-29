0c310e9671941731ab7f09b8e0741880cb48ff10
Hyoung-Seok <dkavmdk98@gmail.com>
2025-07-29 23:36:20 +0900

    [Custom] 사이즈 변경 컨트롤러 작성

---
+++ Assets/2.Scripts/UnityStudy/CustomEditor/SizeController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SizeController : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    [SerializeField] private Vector3 scale;
+    
+    public void ChangeScale()
+    {
+        obj.transform.localScale = scale;
+    }
+}
