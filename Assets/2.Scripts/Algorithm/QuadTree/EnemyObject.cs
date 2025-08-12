54a6670d61a84234b7b00f18c320006e5a489b90
Hyoung-Seok <dkavmdk98@gmail.com>
2025-08-13 01:23:32 +0900

    [Quad] 적 오브젝트 오류 수정

--- Assets/2.Scripts/Algorithm/QuadTree/EnemyObject.cs
+++ Assets/2.Scripts/Algorithm/QuadTree/EnemyObject.cs
@@ -3,9 +3,35 @@ using UnityEngine;
 
 public class EnemyObject : MonoBehaviour
 {
+    [Header("Mat")] 
+    [SerializeField] private Material enableMat;
+    [SerializeField] private Material disableMat;
+
+    private Renderer _renderer;
+    
     private void Update()
     {
         var obj = new GameObject("Empty");
         Destroy(obj);
+    }
+
+    private void OnEnable()
+    {
+        if (_renderer == null)
+        {
+            _renderer = gameObject.GetComponent<Renderer>();
+        }
+        
+        _renderer.material = enableMat;
+    }
+
+    private void OnDisable()
+    {
+        if (_renderer == null)
+        {
+            return;
+        }
+        
+        _renderer.material = disableMat;
     }
 }
