e56c719874ce0bcf787b863cbe14c32fbb6bcb0b
Hyoung-Seok <dkavmdk98@gmail.com>
2025-08-12 23:36:53 +0900

    [Quad] 연산 확인용 코드 추가

--- Assets/2.Scripts/Algorithm/QuadTree/EnemyObject.cs
+++ Assets/2.Scripts/Algorithm/QuadTree/EnemyObject.cs
@@ -1,16 +1,11 @@
+using System;
 using UnityEngine;
 
 public class EnemyObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var obj = new GameObject("Empty");
+        Destroy(obj);
     }
 }
